0e9ecaf533359f3f9377c1932f4c3ad85dd51af1
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-06 21:13:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,6 @@
 array = ["Tim Jones", "Tom Smith", "Jim Camp"]
-def my_collect (array) do |name| 
-  yield(name.split(" ").first) 
-  return array 
-end 
+def my_collect (array) do |name|
+  yield(name.split(" ").first)
+  return array
+end
   
