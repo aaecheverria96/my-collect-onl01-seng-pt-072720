9138f9a856b19f8334285bbd6e4a7c90a32a5e17
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-06 21:41:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
-def my_collect (collection) 
-  while i < collection.length 
-    collection.each do |name| 
-      yield(name.split(" ").first 
+def my_collect (collection)
+  while i < collection.length
+    collection.each do |name|
+      yield(name.split(" ").first
     return name
-  end 
-end 
+  end
+end
